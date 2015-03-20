8f0732e114f84a2b9b33eccd3c19d3ff53ca894a
greenzest <zest.apple@gmail.com>
2015-03-20 10:46:29 +0000

    2

--- 4/app.js
+++ 4/app.js
@@ -1,18 +1,28 @@
 var express = require("express"),
     app = express();
-    url = require("url")
-    
+    url = require('url'),
+    orm = require('orm');
 var animals = ['cat','dog'];
 
-app.use(function(req,res,next){
-    console.log(url.parse(req.url)pathname);
-    console.log();
-    console.log();
-    next();
-});
+app.use(orm.express("sqlite:db.sqlite",{
+    define: function (db,models,next) {
+        models.animal = db.define('animal', {
+            name: String,
+            paws: Number,
+            gender: String,
+            eats: String
+        });
+        models.human = db.define('human', {
+            name: String,
+            sex: String,
+            beard: Boolean
+        });
+        next();
+    }
+}));
 
 app.get('/', function(req,res){
-    res.end('woohoo')
+    res.end(animals.join(', '))
 });
 
 app.get('/new_animal/:name', function(req,res){
